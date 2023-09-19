1f0357626ccb275031e8e68540423a1919e71909
Miguel Angel Sanchez Chordi <mangel.snc@gmail.com>
2023-09-19 20:32:05 +0200

    Add search by name and IP

--- htbmachines.sh
+++ htbmachines.sh
@@ -25,14 +25,10 @@ function helpPanel() {
   echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Usage:${endColour}"
   echo -e "\t${purpleColour}m)${endColour} ${grayColour}Search by machine name${endColour}"
   echo -e "\t${purpleColour}u)${endColour} ${grayColour}Update machines${endColour}"
+  echo -e "\t${purpleColour}i)${endColour} ${grayColour}Search by IP${endColour}"
   echo -e "\t${purpleColour}h)${endColour} ${grayColour}Show help${endColour}\n"
 }
 
-function searchMachine() {
-  machineName="$1"
-
-  echo "$machineName"
-}
 
 function updateMachines() {
   tput civis
@@ -60,13 +56,31 @@ function updateMachines() {
   tput cnorm
 }
 
+function searchMachine() {
+  MACHINE_NAME="$1"
+
+  echo -e "${yellowColour}[+]${endColour} ${grayColour}Showing machine${endColour} ${blueColour}$MACHINE_NAME${endColour}\n"
+
+  cat bundle.js | awk "/name: \"$MACHINE_NAME\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta:" | tr -d '"' | sed 's/dificultad/dificulty/g' | tr -d ',' | sed 's/^ *//'
+}
+
+function searchByIP() {
+  IP_ADDRESS=$1
+
+  MACHINE_NAME=$( cat bundle.js | grep "ip: \"$IP_ADDRESS\"" -B 3 | grep "name:" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')
+
+  echo -e "${yellowColour}[+]${endColour} ${grayColour}The machine name is${endColour} ${blueColour}$MACHINE_NAME${endColour}\n"
+}
+
+
 # Indicators
 declare -i PARAMETER_COUNTER=0
 
-while getopts "m:uh" ARG; do
+while getopts "m:ui:h" ARG; do
   case $ARG in
     m) MACHINE_NAME=$OPTARG; let PARAMETER_COUNTER+=1;;
     u) let PARAMETER_COUNTER+=2;;
+    i) IP_ADDRESS=$OPTARG; let PARAMETER_COUNTER+=3;;
     h) ;;
   esac
 done
@@ -75,6 +89,8 @@ if [ $PARAMETER_COUNTER -eq 1 ]; then
   searchMachine $MACHINE_NAME
 elif [ $PARAMETER_COUNTER -eq 2 ]; then
   updateMachines
+elif [ $PARAMETER_COUNTER -eq 3 ]; then
+  searchByIP $IP_ADDRESS
 else
   helpPanel
 fi
